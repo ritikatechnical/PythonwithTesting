bcb79d1eea76a10857cf30de60b9dc7ce0471089
ritikatechnical <ritikabsr@gmail.com>
2024-07-02 03:58:19 +0530

    *arguments

--- function/17_juneEx4.py
+++ function/17_juneEx4.py
@@ -9,7 +9,7 @@ with_argument("abc", 45, 67, "hello")
 def make_pizza(*toppings):
     """Print the list of toppings that have been requested."""
     for topping in toppings:
-        print("topping:", topping)
+        print(topping, end='\n')
 
 
 make_pizza("tomato", "Olive", "Cheese")

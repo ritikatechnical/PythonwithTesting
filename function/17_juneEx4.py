09a60331931902480a29691049ee69b7f81698e9
ritikatechnical <ritikabsr@gmail.com>
2024-07-02 03:54:40 +0530

    *arguments

---
+++ function/17_juneEx4.py
@@ -0,0 +1,15 @@
+def with_argument(*args):
+    for arg in args:
+        print(arg)
+
+
+with_argument("abc", 45, 67, "hello")
+
+
+def make_pizza(*toppings):
+    """Print the list of toppings that have been requested."""
+    for topping in toppings:
+        print("topping:", topping)
+
+
+make_pizza("tomato", "Olive", "Cheese")
